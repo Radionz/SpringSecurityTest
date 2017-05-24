5b9bb7e389ac849be1d298f1481f6b7aee9a7f32
Dorian Blanc <blanc.dorian.pro@gmail.com>
2017-05-24 12:03:24 +0200

    update 6

--- double/gateway/src/main/java/demo/GatewayApplication.java
+++ double/gateway/src/main/java/demo/GatewayApplication.java
@@ -1,9 +1,5 @@
 package demo;
 
-import java.security.Principal;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -22,60 +19,63 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @SpringBootApplication
 @Controller
 @EnableZuulProxy
 public class GatewayApplication extends SpringBootServletInitializer {
 
-	@RequestMapping("/user")
-	@ResponseBody
-	public Map<String, Object> user(Principal user) {
-		Map<String, Object> map = new LinkedHashMap<String, Object>();
-		map.put("name", user.getName());
-		map.put("roles", AuthorityUtils.authorityListToSet(((Authentication) user)
-				.getAuthorities()));
-		return map;
-	}
+    @RequestMapping("/user")
+    @ResponseBody
+    public Map<String, Object> user(Principal user) {
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("name", user.getName());
+        map.put("roles", AuthorityUtils.authorityListToSet(((Authentication) user)
+                .getAuthorities()));
+        return map;
+    }
 
-	@RequestMapping("/login")
-	public String login() {
-		return "forward:/";
-	}
+    @RequestMapping("/login")
+    public String login() {
+        return "forward:/";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Configuration
-	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    @EnableWebSecurity
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-			// @formatter:off
-			auth.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER")
-			.and()
-				.withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
-			.and()
-				.withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
-// @formatter:on
-		}
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER")
+                    .and()
+                    .withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
+                    .and()
+                    .withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
+        }
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.httpBasic().and()
-				.logout().and()
-				.authorizeRequests()
-					.antMatchers("/index.html", "/login", "/").permitAll()
-					.anyRequest().authenticated()
-					.and()
-				.csrf()
-					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-			// @formatter:on
-		}
-	}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .httpBasic().and()
+                    .logout().and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/login", "/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            // @formatter:on
+        }
+    }
 
 }
